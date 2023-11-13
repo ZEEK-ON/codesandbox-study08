dc842014b4400a9101d996e762c3787917792d15
イワシの匠 <44769766+ZEEK-ON@users.noreply.github.com>
2023-11-13 10:56:12 +0000

    Update 19 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+// import { BrowserRouter, Router } from "react-router-dom";
+// import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+// import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+// import { SearchInput } from "./components/molecules/SearchInput";
+// import { UserCard } from "./components/organisms/user/UserCard";
+// import { DefaultLayout } from "./components/templates/DefaultLayout";
+// import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
+
+// const user = {
+//   name: "イワシ",
+//   image: "https://source.unsplash.com/NE0XGVKTmcA",
+//   email: "12345@example.com",
+//   phone: "090-1111-1111",
+//   company: {
+//     name: "テスト株式会社",
+//   },
+//   website: "https://google.com",
+// };
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router />
+    // <BrowserRouter>
+    //   <DefaultLayout>
+    //     <PrimaryButton>テスト</PrimaryButton>
+    //     <SecondaryButton>検索</SecondaryButton>
+    //     <br />
+    //     <SearchInput />
+    //     <UserCard user={user} />
+    //   </DefaultLayout>
+    // </BrowserRouter>
   );
 }
---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  color: #fff;
+  padding: 6px 24px;
+  border: none;
+  border-radius: 100vmax;
+  outline: none;
+  &:hover {
+    cursor: poninter;
+    opacity: 0.8;
+  }
+`;
